639299656e0215a69c1364699b91e92e5a183680
Darshan <chhantyaldarshan2@gmail.com>
2026-02-09 13:07:28 +0545

    n-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 // import LoginPage from "./components/LoginPage"
-// import LoginPage2 from "./components/LoginPage2";
+import LoginPage2 from "./components/LoginPage2";
 // import RespLoginPage from "./components/RespLoginPage"
-import Test from "./components/Login3";
+// import Test from "./components/Login3";
 
 const App = () => {
   return (
     <>
-      {/* <LoginPage2 /> */}
+      <LoginPage2 />
       {/* <RespLoginPage/> */}
       {/* <LoginSignup/> */}
-      <Test/>
+      {/* <Test/> */}
     </>
   );
 };
--- src/components/LoginPage2.tsx
+++ src/components/LoginPage2.tsx
@@ -4,15 +4,15 @@ const LoginPage2 = () => {
       className="w-screen h-screen bg-gradient-to-tr from-sky-200 via-purple-200 to-pink-200
  flex justify-center items-center"
     >
-      <div className="w-75/100 h-75/100 border-none rounded-2xl shadow-2xl flex flex-wrap ">
+      <div className="w-[95%] h-[50%] md:w-75/100 md:h-75/100 border-none rounded-2xl shadow-2xl flex ">
         <div className="border-none h-full rounded-l-2xl  w-60/100 bg-[url(/156156-abstract-lavender-background.jpg)] bg-cover flex flex-col justify-center items-center gap-3">
-          <h1 className="text-gray-800 font-medium"> Welcome back! </h1>
-          <div className="text-gray-800 font-medium">
+          <h1 className="text-gray-800 md:font-medium font-semibold text-center "> Welcome back! </h1>
+          <div className="text-gray-800 md:font-medium font-light text-center">
             You can sign in to access with your existing account.
           </div>
         </div>
         <div className="border-none h-full sm:w:full md:w-40/100 flex flex-col justify-center items-center gap-4 rounded-r-2xl bg-white">
-          <h1 className="text-gray-800 w-73/100 font-medium">Sign In</h1>
+          <h1 className="text-gray-800 md:w-73/100 md:font-medium my-3">Sign In</h1>
           <form
             action=""
             className="w-full border-none  flex flex-col items-center h-1/2 gap-4 "
@@ -20,24 +20,24 @@ const LoginPage2 = () => {
             <input
               type="email"
               placeholder="Username or email"
-              className="border border-gray-800 w-75/100 h-22/100 rounded-3xl px-3 placeholder-gray-800"
+              className="border border-gray-800 w-75/100 h-22/100 rounded-3xl px-3 placeholder-gray-800 placeholder: text-sm"
             />
             <input
               type="text"
               placeholder="Password"
-              className="border border-gray-800 w-75/100 h-22/100 rounded-3xl px-3 placeholder-gray-800"
+              className="border border-gray-800 w-75/100 h-22/100 rounded-3xl px-3 placeholder-gray-800 placeholder: text-sm"
             />
             <div className="flex w-75/100 justify-between">
               {" "}
-              <div className="text-gray-800">
+              <div className="text-gray-800 md:block hidden">
                 {" "}
                 <input
                   type="checkbox"
-                  className=" accent-purple-500 cursor-pointer w-3 h-3 border border-gray-800"
+                  className=" accent-purple-500 cursor-pointer w-3 h-3 border border-gray-800 "
                 />{" "}
                 Remember me{" "}
               </div>
-              <a href="" className="text-purple-500">
+              <a href="" className="text-purple-500 md:m-0 mx-5">
                 Forgot password?
               </a>
             </div>
@@ -45,7 +45,7 @@ const LoginPage2 = () => {
               Sign In
             </button>
           </form>
-          <div className="text-gray-800">
+          <div className="text-gray-800 p-3">
             New here?{" "}
             <a href="" className="text-purple-500">
               Create an Account
