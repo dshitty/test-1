efbc1a49c1259ce8a8699376513317b06bd2243c
Darshan <chhantyaldarshan2@gmail.com>
2026-02-09 12:27:14 +0545

    5th commit

--- src/components/Login3.tsx
+++ src/components/Login3.tsx
@@ -28,12 +28,12 @@ const Test = () =>{
   };
     return(
         <>
-        <div className="w-screen h-screen bg-[#E0F4F0]   flex justify-center items-center gap-4">
-            <div className="w-[70%] h-[70%] border rounded-2xl bg-white flex relative shadow-[0_25px_60px_-15px_rgba(0,0,0,0.25)] overflow-hidden">
+        <div className="w-screen h-screen bg-[#E0F4F0]   flex justify-center items-center gap-4 ">
+            <div className="w-[90%] h-[55%] md:w-[70%] md:h-[70%] border rounded-2xl bg-white flex flex-wrap   relative shadow-[0_25px_60px_-15px_rgba(0,0,0,0.25)] overflow-hidden">
                 <div className={`flex flex-col justify-center items-center gap-4 w-[35%] h-full   rounded-l-2xl bg-gradient-to-br from-[#2FB7A4] to-[#27A896] z-2 absolute ${slide?'left-[65%] rounded-r-2xl rounded-l-none':'left-0'} ${pop?'scale-x-[2.5]':'scale-x-[1]'} transition-all ease-in duration-700 `}>
-                 { slide?  <h1 className={`text-3xl font-semibold text-white ${fadeOut?'opacity-0':'opacity-100'} transition-all duration-500`}>Hello, Friend</h1>:<h1 className={`text-3xl font-semibold text-white ${fadeOut?'opacity-0':'opacity-100'} transition-all duration-500`}>Welcome Back!</h1>}
+                 { slide?  <h1 className={` text-2xl text-center md:text-3xl font-semibold text-white ${fadeOut?'opacity-0':'opacity-100'} transition-all duration-500`}>Hello, Friend</h1>:<h1 className={`text-2xl text-center md:text-3xl font-semibold text-white ${fadeOut?'opacity-0':'opacity-100'} transition-all duration-500`}>Welcome Back!</h1>}
                   { slide? <div className={`text-center p-3 text-white ${fadeOut?'opacity-0':'opacity-100'} transition-all duration-500`}>Enter your personal details and start journey with us</div>:<div className={`text-center p-3 text-white  ${fadeOut?'opacity-0':'opacity-100'} transition-all duration-500`}>To keep connected with us please login with your personal info</div>}
-                  { slide?<button onClick={handleSlide} className={`bg-green-600 border-white rounded-4xl ${fadeOut?'opacity-0':'opacity-100'} transition-all duration-500 `}>SIGN UP</button>:<button onClick={handleSlide} className={`bg-green-600 border-white rounded-4xl ${fadeOut?'opacity-0':'opacity-100'} transition-all duration-500 `}>SIGN IN</button>}
+                  { slide?<button onClick={handleSlide} className={`bg-gradient-to-br from-[#2FB7A4] to-[#27A896] z-2 border-white rounded-4xl ${fadeOut?'opacity-0':'opacity-100'} transition-all duration-500 `}>SIGN UP</button>:<button onClick={handleSlide} className={`bg-gradient-to-br from-[#2FB7A4] to-[#27A896] z-2 border-white rounded-4xl ${fadeOut?'opacity-0':'opacity-100'} transition-all duration-500 `}>SIGN IN</button>}
                 </div>
                 {hide &&  <div className={`w-[65%] h-full  bg-white rounded-r-2xl absolute hidden ${slide?'right-[35%] rounded-l-none':'right-0'} transition-all ease-in duration-900 flex flex-col justify-evenly items-center`}>
                     <div className={` text-black flex flex-col justify-center items-center gap-5  `}>
@@ -54,7 +54,7 @@ const Test = () =>{
                 </div> }
             {  hide?   <div className={`w-[65%] h-full  bg-white absolute ${slide?'right-[35%]':'right-0'} transition-all ease-in duration-900 flex flex-col justify-evenly items-center`}>
                     <div className={` text-black flex flex-col justify-center items-center gap-5  `}>
-                        <h1 className={`text-[#1F8A70] font-semibold text-4xl  transition-all `}>Sign in to Whatever</h1>
+                        <h1 className={`text-[#1F8A70] font-semibold text-3xl text-center md:text-4xl  transition-all `}>Sign in to Whatever</h1>
                         <div className="flex justify-center gap-2">
                             <div className={`border border-gray-300 rounded-[100%] w-9 text-center transition-all `}>fa</div>
                             <div className={`border  border-gray-300 rounded-[100%] w-9 text-center  transition-all `}>ga</div>
@@ -65,24 +65,24 @@ const Test = () =>{
                     <form action="" className=" flex flex-col justify-center items-center gap-3 w-[60%]">
                         <input type="email" placeholder="Email" className={`placeholder-gray-400 bg-gray-100  h-12 w-full p-3  transition-all `}/>
                         <input type="text" placeholder="Password"  className={`placeholder-gray-400 bg-gray-100  h-12 w-full p-3  transition-all `}/>
-                        <div className={`text-black  transition-all `}>Forgot your password?</div>
-                        <button className={`w-[60%] rounded-3xl font-bold bg-gradient-to-br from-[#2FB7A4] to-[#27A896] transition-all hover:from-[#28A194] hover:to-[#1F9186] hover:shadow-lg hover:scale-105 `}>SIGN IN</button>
+                        <div className={`text-black  transition-all text-center `}>Forgot your password?</div>
+                        <button className={`md:w-[60%] rounded-3xl font-bold bg-gradient-to-br from-[#2FB7A4] to-[#27A896] transition-all hover:from-[#28A194] hover:to-[#1F9186] hover:shadow-lg hover:scale-105 `}>SIGN IN</button>
                     </form>
-                </div> :<div className={`w-[65%] h-full  bg-white rounded-r-2xl absolute ${slide?'right-[35%]':'right-0'} transition-all ease-in duration-900 flex flex-col justify-evenly items-center`}>
+                </div> :<div className={`w-[65%] h-full  bg-white rounded-r-2xl absolute ${slide?'right-[35%]':'right-0'} transition-all ease-in duration-900 flex flex-col justify-evenly items-center `}>
                     <div className=" text-black flex flex-col justify-center items-center gap-5 ">
-                        <h1 className={`text-[#1F8A70] font-semibold text-4xl  transition-all `}>Create Account</h1>
+                        <h1 className={`text-[#1F8A70] font-semibold text-3xl md:text-4xl  transition-all `}>Create Account</h1>
                         <div className="flex justify-center gap-2">
                             <div className={`border border-gray-300 rounded-[100%] w-9 text-center  transition-all `}>fa</div>
                             <div className={`border  border-gray-300 rounded-[100%] w-9 text-center  transition-all `}>ga</div>
                             <div className={`border  border-gray-300 rounded-[100%] w-9 text-center  transition-all `}>inn</div>
                         </div>
-                        <div className={`text-gray-600  transition-all `}>or use your email for registration</div>
+                        <div className={`text-gray-600  transition-all text-center `}>or use your email for registration</div>
                     </div>
                     <form action="" className=" flex flex-col justify-center items-center gap-3 w-[60%]">
                         <input type="text" placeholder="Name" className={`placeholder-gray-400 bg-gray-100  h-12 w-full p-3  transition-all duration-500`}/>
                         <input type="email" placeholder="Email" className={`placeholder-gray-400 bg-gray-100  h-12 w-full p-3  transition-all duration-500`}/>
                         <input type="text" placeholder="Password"  className={`placeholder-gray-400 bg-gray-100  h-12 w-full p-3  transition-all duration-500`}/>
-                        <button className={`w-[60%] rounded-3xl font-bold bg-gradient-to-br from-[#2FB7A4] to-[#27A896]  transition-all duration-500 hover:from-[#28A194] hover:to-[#1F9186] hover:shadow-lg hover:scale-105`}>SIGN UP</button>
+                        <button className={` md:w-[60%] rounded-3xl font-bold bg-gradient-to-br from-[#2FB7A4] to-[#27A896]  transition-all duration-500 hover:from-[#28A194] hover:to-[#1F9186] hover:shadow-lg hover:scale-105`}>SIGN UP</button>
                     </form>
                 </div>} 
                  
